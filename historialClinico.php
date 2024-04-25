431656200f490041912b91d77078efa414c30d3c
Felipe Rodríguez <Danitronick22@gmail.com>
2024-04-25 01:36:18 -0500

    Commit cita

--- historialClinico.php
+++ historialClinico.php
@@ -8,6 +8,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+<ul class="nav nav-tabs" style=" padding: 1em; background: #fff4c1c2;">
+    <li class="nav-item">
+      <a class="nav-link myitem"  aria-current="page"  href="Registro.php" onclick="mostrarTabla('clientes')">Registrar Paciente</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link myitem" href="historialClinico.php">Historial de cliente</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link myitem" href="#" onclick="mostrarTabla('casos')">Factura</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link"style="border: none; color: #ffc108; font-variant-caps: all-petite-caps; font-weight: 900; letter-spacing: 1px;" href="cita.php" onclick="mostrarTabla('casos')">cita</a>
+    </li>
+  </ul>
   <div class="conteiner">
     <div class="content">
       <!-- Tabla de clientes -->
