dc41e9162052b022ab19316b7e69b60d3b766539
sara <saradan.vm@gmail.com>
2024-04-24 22:53:30 -0500

    cambios en registro y php registrocon

--- Registro.php
+++ Registro.php
@@ -1,41 +1,4 @@
-<?php
-    // Incluimos conexión
-    include 'conexion.php';
 
-    if(isset($_POST['crear'])){
-
-        $cedula = mysqli_real_escape_string($con, $_POST['cedula']);
-        $nombre = mysqli_real_escape_string($con, $_POST['nombre']);
-        $email = mysqli_real_escape_string($con, $_POST['email']);
-        $telefono = mysqli_real_escape_string($con, $_POST['telefono']);
-        $direccion = mysqli_real_escape_string($con, $_POST['direccion']);
-
-        $nombreA = mysqli_real_escape_string($con, $_POST['nombreA']);
-        $fechaini = mysqli_real_escape_string($con, $_POST['fechaini']);
-        $raza = mysqli_real_escape_string($con, $_POST['raza']);
-        $tamanio = mysqli_real_escape_string($con, $_POST['tamanio']);
-        $color = mysqli_real_escape_string($con, $_POST['color']);
-        $especie = mysqli_real_escape_string($con, $_POST['especie']);
-
-        if(!isset($cedula) || $cedula == '' || !isset($nombre) || $nombre == '' || !isset($telefono) || $telefono == '' || !isset($email) || $email == '' || !isset($direccion) || $direccion == '' || !isset($nombreA) || $nombreA == '' || !isset($fechaini) || $fechaini == '' || !isset($raza) || $raza == '' || !isset($tamanio) || $tamanio == '' || !isset($color) || $color == '' || !isset($especie) || $especie == ''){
-          $error = "Algunos campos están vacíos";
-      }else{
-          $queryCliente = "INSERT INTO cliente(cedula, nombre, email, telefono, direccion)VALUES('$cedula', '$nombre', '$email', '$telefono', '$direccion')";
-          $queryAnimal = "INSERT INTO animal(nombreA, raza, fechaini, tamanio, color, especie)VALUES('$nombreA', '$raza', '$fechaini', '$tamanio', '$color', '$especie')";
-
-          if(!mysqli_query($con, $queryCliente) || mysqli_query($con, $queryAnimal)){
-              die('Error: ' . mysqli_error($con));
-              $error = "Error, no se pudo crear el registro";
-          }else{
-              $mensaje = "Registro creado correctamente";
-              header('Location: index.php?mensaje='.urlencode($mensaje));
-              exit();
-          }
-      }
-      
-    }
-
-?>
 
 
 <!DOCTYPE html>
@@ -57,7 +20,7 @@
   </head>
 <body>
   <div class="containerr text-center">
-  <form class="conteiner-form" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+  <form class="conteiner-form" method="POST" action="registrocon.php">
 
     <div style="height: 100vh;display: flex;" class="row content align-items-center">      
       <div class="col col-5 clientes">
---
+++ registrocon.php
@@ -0,0 +1,38 @@
+<?php
+    // Incluimos conexión
+    include 'conexion.php';
+
+    if(isset($_POST['crear'])){
+
+        $cedula = mysqli_real_escape_string($con, $_POST['cedula']);
+        $nombre = mysqli_real_escape_string($con, $_POST['nombre']);
+        $email = mysqli_real_escape_string($con, $_POST['email']);
+        $telefono = mysqli_real_escape_string($con, $_POST['telefono']);
+        $direccion = mysqli_real_escape_string($con, $_POST['direccion']);
+
+        $nombreA = mysqli_real_escape_string($con, $_POST['nombreA']);
+        $fechaini = mysqli_real_escape_string($con, $_POST['fechaini']);
+        $raza = mysqli_real_escape_string($con, $_POST['raza']);
+        $tamanio = mysqli_real_escape_string($con, $_POST['tamanio']);
+        $color = mysqli_real_escape_string($con, $_POST['color']);
+        $especie = mysqli_real_escape_string($con, $_POST['especie']);
+
+        if(!isset($cedula) || $cedula == '' || !isset($nombre) || $nombre == '' || !isset($telefono) || $telefono == '' || !isset($email) || $email == '' || !isset($direccion) || $direccion == '' || !isset($nombreA) || $nombreA == '' || !isset($fechaini) || $fechaini == '' || !isset($raza) || $raza == '' || !isset($tamanio) || $tamanio == '' || !isset($color) || $color == '' || !isset($especie) || $especie == ''){
+          $error = "Algunos campos están vacíos";
+      }else{
+          $queryAnimal = "INSERT INTO animal(nombreA, raza, fechaini, tamanio, color, especie)VALUES('$nombreA', '$raza', '$fechaini', '$tamanio', '$color', '$especie')";
+          $queryCliente = "INSERT INTO cliente(cedula, nombre, email, telefono, direccion)VALUES('$cedula', '$nombre', '$email', '$telefono', '$direccion')";
+
+          if(!mysqli_query($con, $queryCliente) || !mysqli_query($con, $queryAnimal)){
+              die('Error: ' . mysqli_error($con));
+              $error = "Error, no se pudo crear el registro";
+          }else{
+              $mensaje = "Registro creado correctamente";
+              header('Location: index.php?mensaje='.urldecode($mensaje));
+              exit();
+          }
+      }
+    
+    }
+
+?>
