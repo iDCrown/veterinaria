cafc1a42f1aa962ae44edbb88f318341c4e3e00a
Felipe Rodríguez <Danitronick22@gmail.com>
2024-06-02 21:07:25 -0500

    Modal muestra datos completos de la factura

--- factura.php
+++ factura.php
@@ -6,12 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Factura</title>
   <link rel="stylesheet" href="./css/historial.css">
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-      crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
   <!-- nav -->
@@ -19,23 +14,19 @@
     <li class="nav-item">
       <a class="nav-link myitem" style="border: none;color: #aee570;font-weight: 900;font-size: 23px;font-family: serif;font-variant-caps: all-petite-caps;" aria-current="page"  href="index.php">Inicio</a>
     </li>    
-
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" style="border: none;color: #aee570;font-weight: 900;font-size: 23px;font-family: serif;font-variant-caps: all-petite-caps;" aria-current="page" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
       Registrar Paciente
       </a>
       <ul class="dropdown-menu">
-        <li>
-          <a class="dropdown-item" style="border: none;color: #aee570;font-weight: 900;font-size: 20px;font-family: serif;font-variant-caps: all-petite-caps;" href="Registro.php">Nueva Mascota</a>
-        </li>
-        <li>
-          <a class="dropdown-item" style="border: none;color: #aee570;font-weight: 900;font-size: 20px;font-family: serif;font-variant-caps: all-petite-caps;" href="RegistroMascota.php">Agregar otra Mascota</a>
-        </li>
+        <li><a class="dropdown-item" style="border: none;color: #aee570;font-weight: 900;font-size: 20px;font-family: serif;font-variant-caps: all-petite-caps;" href="Registro.php">Nueva Mascota</a></li>
+        <li><a class="dropdown-item" style="border: none;color: #aee570;font-weight: 900;font-size: 20px;font-family: serif;font-variant-caps: all-petite-caps;" href="RegistroMascota.php">Agregar otra Mascota</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
       </ul>
     </li>
-
     <li class="nav-item">
-      <a class="nav-link" style="border: none;color: #aee570;font-weight: 900;font-size: 23px;font-family: serif;font-variant-caps: all-petite-caps;" href="cita.php">cita</a>
+      <a class="nav-link" style="border: none;color: #aee570;font-weight: 900;font-size: 23px;font-family: serif;font-variant-caps: all-petite-caps;" href="cita.php">Cita</a>
     </li>
     <li class="nav-item">
       <a class="nav-link myitem" style="border: none;color: #aee570;font-weight: 900;font-size: 23px;font-family: serif;font-variant-caps: all-petite-caps;" href="historialClinico.php">Historial de cliente</a>
@@ -43,19 +34,19 @@
     <li class="nav-item">
       <a class="nav-link myitem" style="border: none;color: #aee570;font-weight: 900;font-size: 23px;font-family: serif;font-variant-caps: all-petite-caps;" href="factura.php">Factura</a>
     </li>
-  </ul> 
+  </ul>  
   <!-- historial clinico -->
   <div class="conteiner">
     <h1 class="title">Factura</h1>
     <div class="content">
       <form class="consultar" action="" method="GET">
-      <p  style="color:black" class="p_crear">Ingrese el ID del cliente</p>
+        <p  style="color:black" class="p_crear">Ingrese el ID del cliente</p>
         <input class="input" type="text" name="cedula">
-        <button type="submit" class="button" name="consultar">buscar</button>
+        <button type="submit" class="button" name="consultar">Buscar</button>
       </form>
       <!-- Tabla de clientes -->
       <div id="clientes" style="display: block;">
-        <table class="table table-hover ">
+        <table class="table table-hover">
           <thead class="table table-bordered b3">
             <tr>
               <th class="th_color" scope="col">Factura Nº</th>
@@ -74,19 +65,16 @@
               <td scope="row"><?php echo $row['estado']; ?></td>
               <td scope="row"><?php echo $row['nombreA']; ?></td>
               <td>
-              <form method="POST" action="">
-                <input type="hidden" name="cedula" value="">
-                <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="button">Pagar</button>
-                <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="button">ver</button>
-              </form>
-            </td>
+                <button type="button" class="btnVer button" data-factura="<?php echo $row['Factura_Nº']; ?>" data-bs-toggle="modal" data-bs-target="#exampleModal">Ver</button>
+                <button type="button" class="btnPagar button" data-factura="<?php echo $row['Factura_Nº']; ?>" data-bs-toggle="modal" data-bs-target="#exampleModal">Pagar</button>
+              </td>
             </tr>
             <?php endwhile; ?>
-            <?php else: ?>
-              <tr>
-                <td colspan="5">No se encontraron facturas.</td>
-              </tr>
-            <?php endif; ?>
+          <?php else: ?>
+            <tr>
+              <td colspan="5">No se encontraron facturas.</td>
+            </tr>
+          <?php endif; ?>
           </tbody>
         </table>
       </div>
@@ -98,76 +86,63 @@
       <div class="modal-content">
         <div class="modalFactura-header">
           <div>
-            <h1 class="modalFactura-title " id="exampleModalLabel">Veterinaria</h1>
-            <span class="fechaEmicion">17 june 2021</span>
+            <h1 class="modalFactura-title" id="exampleModalLabel">Veterinaria</h1>
           </div>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="tableFactura-header">
           <div class="infoMascota">
-            <h2 class="title_mascota">Andres</h2>
+            <h2 class="title_mascota" id="nombreMascota"></h2>
             <table class="data-table">
-                <tr>
-                  <th>Tipo:</th><td>Calle Falsa 123, Ciudad</td>
-                </tr>
-                <tr>
-                  <th>Raza:</th><td>Calle Falsa 123, Ciudad</td>
-                </tr>
-                <tr>
-                  <th>Especie:</th><td>Calle Falsa 123, Ciudad</td>
-                </tr>
+              <tr>
+                <th>Color:</th><td id="tipoMascota"></td>
+              </tr>
+              <tr>
+                <th>Raza:</th><td id="raza"></td>
+              </tr>
+              <tr>
+                <th>Especie:</th><td id="especie"></td>
+              </tr>
             </table>
           </div>
           <div class="column_Nfactura">
             <table class="data-table">
               <tr>
-                <th class="facturaN">Factura Nº:</th><td>5245</td>
+                <th class="facturaN">Factura Nº:</th><td id="facturaN"></td>
               </tr>
               <tr>
-                <th class="facturaN">Fecha de Emisión:</th><td>02/MAY/2024</td>
+                <th class="facturaN">Fecha de Emisión:</th><td id="fechaEmision"></td>
               </tr>
             </table>
           </div>
         </div>
         <div class="tableFactura-header">
-        <table class="table">
+          <table class="table">
             <thead>
               <tr>
                 <th style="color: #a4e404;" scope="col">Descripción</th>
                 <th style="color: #a4e404;" scope="col">Precio Unit</th>
               </tr>
             </thead>
-            <tbody class="table-group-divider">
-              <tr>
-                <td style=" width: 28em;">Consulta General</td>
-                <td>$50.00</td>
-              </tr>
-              <tr>
-                <td style=" width: 28em;">Vacuna Antirrábica</td>
-                <td>$30.00</td>
-              </tr>
-              <tr>
-                <td style=" width: 28em;">Desparasitante</td>
-                <td>$20.00</td>
-              </tr>
+            <tbody class="table-group-divider" id="detalleServicios">
+              <!-- Los detalles de los servicios se agregarán aquí -->
             </tbody>
           </table>
         </div>
         <div class="tableFactura-header total_factura">
-        <table class="table">
-            
+          <table class="table">
             <tbody class="table-group-divider">
               <tr>
-                <td style=" width: 28em;">Subtotal</td>
-                <td>$100.00</td>
+                <td style="width: 28em;">Subtotal</td>
+                <td id="subtotal">$0.00</td>
               </tr>
               <tr>
-                <td style=" width: 28em;" >IVA</td>
-                <td>10%</td>
+                <td style="width: 28em;">IVA</td>
+                <td id="iva">10%</td>
               </tr>
               <tr>
-                <td style=" width: 28em;">Total</td>
-                <td>$110.00</td>
+                <td style="width: 28em;">Total</td>
+                <td id="total">$0.00</td>
               </tr>
             </tbody>
           </table>
@@ -179,6 +154,41 @@
     </div>
   </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('.btnVer, .btnPagar').on('click', function() {
+        var numeroFactura = $(this).data('factura');
+
+        $.ajax({
+          url: 'facturacon.php',
+          type: 'POST',
+          data: { numero_factura: numeroFactura },
+          success: function(response) {
+            var data = JSON.parse(response);
+
+            $('#fechaEmision').text(data.fecha_Emision);
+            $('#nombreMascota').text(data.nombreMascota);
+            $('#tipoMascota').text(data.tipoMascota);
+            $('#raza').text(data.raza);
+            $('#especie').text(data.especie);
+            $('#facturaN').text(numeroFactura);
+
+            var servicios = '';
+            var subtotal = 0;
+            data.servicios.forEach(function(servicio) {
+              servicios += '<tr><td style="width: 28em;">' + servicio.Nombre_Servicio + '</td><td>' + servicio.Precio_Unitario + '</td></tr>';
+              subtotal += parseFloat(servicio.Precio_Unitario);
+            });
+
+            $('#detalleServicios').html(servicios);
+            $('#subtotal').text('$' + subtotal.toFixed(2));
+            $('#total').text('$' + (subtotal * 1.10).toFixed(2)); // Assuming 10% IVA
+          }
+        });
+      });
+    });
+  </script>
 </body>
 </html>
--- facturacon.php
+++ facturacon.php
@@ -4,23 +4,6 @@ $result = null;
 
 if (isset($_GET['cedula'])) {
   $idCedula = $_GET['cedula'];
-
-  // $query_factura = 
-  // "SELECT c.cedula AS cliente_id, c.nombre AS cliente_nombre, 
-  //         m.idMacota AS mascota_id, m.nombreA AS mascota_nombre, 
-  //         s.idServicio AS servicio_id, s.Nombre_Servicio AS servicio_nombre, s.precio,
-  //         d.detallehistorialclinico AS 
-  // FROM mascotas m
-  // JOIN clientes c ON m.cliente_id = c.id
-  // JOIN servicios_mascota sm ON m.id = sm.mascota_id
-  // JOIN servicios s ON sm.servicio_id = s.id
-  // WHERE c.cedula = ?";
-
-  // $stmt = $con->prepare($query_factura);
-  // $stmt->bind_param('i', $idCedula); 
-  // $stmt->execute();
-  // $result = $stmt->get_result();
-
   $query_historial_Factura = "SELECT Factura_Nº, Fecha_Emision, nombreA, estado FROM facturas WHERE cedula = ?";
 
   $stmt = $con->prepare($query_historial_Factura);
@@ -29,6 +12,63 @@ if (isset($_GET['cedula'])) {
   $result = $stmt->get_result();
 
 }
+
+
+
+if (isset($_POST['numero_factura'])) {
+  $numeroFactura = $_POST['numero_factura'];
+
+  $query_factura = "SELECT 
+  f.Factura_Nº,
+  f.Fecha_Emision, 
+  m.nombreA AS nombreMascota, 
+  m.raza, 
+  m.color, 
+  m.especie, 
+  s.Nombre_Servicio, 
+  s.Precio_Unitario 
+  FROM facturas f 
+  JOIN historialclinico hc ON f.cedula = hc.cedula 
+  JOIN mascota m ON hc.idMascota = m.idMascota 
+  JOIN cliente c ON m.cedula = c.cedula 
+  JOIN detallehistorialclinico dhc ON hc.idHistorialClinico = dhc.idHistorialClinico 
+  JOIN servicios s ON dhc.idServicio = s.idServicio
+  WHERE f.Factura_Nº = ?";
+
+  $stmt = $con->prepare($query_factura);
+  $stmt->bind_param('i', $numeroFactura);
+  $stmt->execute();
+  $result = $stmt->get_result();
+
+  $factura = [
+    'fecha_Emision' => '',
+    'nombreMascota' => '',
+    'tipoMascota' => '',
+    'raza' => '',
+    'especie' => '',
+    'servicios' => []
+  ];
+
+  if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+      if (empty($factura['fecha_Emision'])) {
+        $factura['fecha_Emision'] = $row['Fecha_Emision'];
+        $factura['nombreMascota'] = $row['nombreMascota'];
+        $factura['tipoMascota'] = $row['color'];
+        $factura['raza'] = $row['raza'];
+        $factura['especie'] = $row['especie'];
+      }
+      $factura['servicios'][] = [
+        'Nombre_Servicio' => $row['Nombre_Servicio'],
+        'Precio_Unitario' => $row['Precio_Unitario']
+      ];
+    }
+  }
+
+  echo json_encode($factura);
+}
+?>
+
 
 
 
